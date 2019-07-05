753d88ac7697dd800892f9e8b37f8ba191631da2
Elizabeth Prendergast <44070190+eprendergast@users.noreply.github.com>
2019-07-05 20:51:58 +0100

    Update scheduling_task.rb

--- scheduling_task.rb
+++ scheduling_task.rb
@@ -1,76 +1,41 @@
 require 'date'
-
-# d = Date.parse('26th July 2019')
-#                              #=> #<Date: 2001-02-03 ...>
-# puts d.year                       #=> 2001
-# d.mon                        #=> 2
-# d.mday                       #=> 3
-# d.wday                       #=> 6
-# d += 1                       #=> #<Date: 2001-02-04 ...>
-# d.strftime('%a %d %b %Y')    #=> "Sun 04 Feb 2001"
-
-#Given an array of dates and an array of bank holiday dates, increment a date by 1 if it falls on a bank holiday
-
 #array of random schedule dates
 dates = [
   '6 July 2019',
-  '21 October 2019',
-  '1 November 2019',
-  '12 November 2019',
-  '18 November 2019',
-  '21 November 2019',
+  '7 July 2019',
   '17 December 2019',
-  '26 December 2019',
+  '25 December 2019',
   '1 January 2020',
-  '8 January 2020',
-  '28 January 2020'
+  '8 January 2020'
 ]
-
-#array of bank holiday dates
+#array of random bank holiday dates
 bank_holidays = [
   '25 December 2019',
   '26 December 2019',
   '1 January 2020'
 ]
-
 #define a method which parses data from an array of dates into ruby datetime format
 def parseDates(array)
-  #create array to hold the parsed dates
-  parsed_dates = []
-  #parse dates into ruby datetime format
-  dates.each do |date|
-    parsed_date = Date.parse(date)
-    parsed_dates.push(parsed_date)
+  parsed_dates = [] #create epmty array to hold the parsed dates
+  array.each do |date| #for each string in the array...
+    parsed_date = Date.parse(date) #parse the date
+    parsed_dates.push(parsed_date) #push the parsed date onto the new parsed_dates array
   end
   parsed_dates
 end
 
-#define a method which accepts two paramets, an array of dates and an array of bank holidays, and return an array of dates corrected for bank holidays
+#define a method which accepts two paramets, an array of dates and an array of bank holidays, and returns an array of dates corrected for bank holidays
 def holidayChecker(dates, bank_holidays)
-  #parse dates array
-  dates = parseDates(dates)
-  #parse bank holidays array
-  bank_holidays = parseDates(bank_holidays)
-
-  parsed_dates.each do |date|
-    if bank_holidays.include?(date)
-      puts "#{date} is a bank holiday!"
-    end
-  end
-
-  #for each date, check...
-    #does the date fall on a weekend?
-  parsed_dates.each do |date|
-    if date.wday == 6
-      date += 2
-    elsif date.wday == 7
+  dates = parseDates(dates) #parse dates array
+  bank_holidays = parseDates(bank_holidays) #parse bank holidays array
+  corrected_schedule = []
+  dates.each do |date|
+    while date.wday == 6 || date.wday == 7 || bank_holidays.include?(date) #while the date is either a bank holiday OR a Saturday OR a Sunday...
       date += 1
-    else
-      puts "It's a weekday."
     end
-
+    corrected_schedule.push(date)
   end
-
+  puts corrected_schedule
 end
 
 holidayChecker(dates, bank_holidays)
